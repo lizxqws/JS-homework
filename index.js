5050d7a71d7954ce68c1f4bf2b4327609a4978ef
lizxqws <goncharov4ento@gmail.com>
2024-02-13 17:17:46 +0100

    Added 9 tasks

---
+++ index.js
@@ -0,0 +1,42 @@
+// task 1
+const age = 14;
+console.log("Мій вік:", age);
+// task 2
+const myName = "Єлизавета";
+console.log("Моє ім´я:", myName);
+// task 3
+const isStudent = false;
+console.log("Чи ви є студентом:", isStudent);
+// task 4
+const myString = "No risk, no story";
+console.log("Моя улюблена цитата:", myString);
+// task 5
+const myNumber = 87;
+console.log("Сума двох чисел:", myNumber + 10);
+// task 6
+const myNull = null;
+console.log("Значення null:", myNull);
+
+// task 7
+
+// let yourName = prompt("Введіть ваше ім´я:");
+// alert("Привіт " + yourName + "!");
+
+// task 8
+
+// let confirmation = confirm("Підтвердити дію?");
+// if (confirmation) {
+//   alert("Дякую за підтвердження!");
+// } else {
+//   alert("Дію відмінено!");
+// }
+
+// task 9
+
+// alert ("Ця дія може бути небезпечною!")
+// let confirmation = confirm("Підтвердити Вашу дію?");
+// if (confirmation) {
+//   alert("Дякую за підтвердження!");
+// } else {
+//   alert("Дію відмінено!");
+// }
