455e120f728fa2eb18e1bffa994b16af3d530f27
lizxqws <goncharov4ento@gmail.com>
2024-02-20 20:07:08 +0100

    Add the third hw

---
+++ index.js
@@ -0,0 +1,17 @@
+// task 1
+const result = 5 + 5 + "5";
+console.log("Завдання номер 1: " + result);
+// task 2
+const myEmail = "goncharov4ento@gmail.com";
+console.log("Чи має змінна символ @: " + myEmail.includes("@"));
+console.log("Скільки символів має змінна: " + myEmail.length);
+// task 3
+const my = "My ";
+const forename = "name ";
+const is = "is ";
+const fullName = my + forename + is + "Viktor";
+console.log(fullName);
+// task 4
+const userName = "Олександро";
+const payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень.`);
